548a7109c55a7e56eb0128e8615d19ecdfe6094d
John Waithaka <johnwaruiwaithaka@gmail.com>
2024-04-18 12:20:26 +0300

    arrow fxns complete

---
+++ index.js
@@ -0,0 +1,17 @@
+//requirements 
+// 1) has a function expression called divide
+// 2) divide divides 2000 by 100
+// 3) has an arrow function called square
+// 4) square arrow function takes one parameter and multiplies it times itself
+// 5) has an arrow function called add
+// 6) add arrow function takes two parameters and adds them together
+
+//test 1 and 2
+const divide = (a, b) => a / b;
+
+//test 3 and 4
+const square = x => x * x;
+
+//test 4 and 50
+const add = (a, b) => a + b;
+
